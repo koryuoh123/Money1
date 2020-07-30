e809e13210ffeb7d90c3a81c0e2f8094df0008b4
koryuoh <526248620@qq.com>
2020-07-30 12:01:08 +0800

    add pages 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+404
+<div><router-link to ="/">返回首页</router-link></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
