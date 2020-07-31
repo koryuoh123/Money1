8f4ade773fd86d12bff22d04301bc081f0aaa8cc
koryuoh <526248620@qq.com>
2020-07-31 15:30:35 +0800

    添加Money的样式

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-    <div>App</div>
+  <div id="x">
     <hr />
-    <router-view />
+    <router-view ></router-view>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  
+};
 </script>
 <style lang="scss">
+
+*{
+  margin: 0; padding: 0;
+  box-sizing: border-box;
+}
+
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
-    Money.vue
-    <Nav/>
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money.vue</p>
+    </div>
+    <Nav />
   </div>
 </template>
 
 <script>
-
 export default {
-  name: "Money",
-  
+  name: "Money"
 };
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  border: 2px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
