c8b832f861373cd3004a754b52cd8fa2826ece3a
koryuoh <526248620@qq.com>
2020-08-06 16:51:15 +0800

    消除了Tags的警告，目前还存在bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -6,10 +6,10 @@
     <ul class="current">
       <li
         v-for="tag in dataSource"
-        :key="tag"
+        :key="tag.id"
         @click="toggle(tag)"
         :class="{selected: selectedTags.indexOf(tag)>=0}"
-      >{{tag}}</li>
+      >{{tag.name}}</li>
     </ul>
   </div>
 </template>
