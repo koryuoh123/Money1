dc254a6a5db79a8293fc2a55eab99c789d403b10
koryuoh <526248620@qq.com>
2020-08-06 18:06:28 +0800

    展示tag的name，可以编辑和删除标签

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,32 +1,68 @@
-const localStorageKeyName = 'tagList';
+const localStorageKeyName = "tagList";
 type Tag = {
-    id: string;
-    name: string;
-  }
+  id: string;
+  name: string;
+};
 
 type TagListModel = {
   data: Tag[];
   fetch: () => Tag[];
-  create: (name: string) => 'success' | 'duplicated'; // 联合类型
+  create: (name: string) => "success" | "duplicated"; // 联合类型
+  update: (id: string, name: string) => "success" | "not found" | "duplicated";
   save: () => void;
-}
+  remove: (id: string) => boolean;
+};
 
 const tagListModel: TagListModel = {
   data: [],
   fetch() {
-    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
+    this.data = JSON.parse(
+      window.localStorage.getItem(localStorageKeyName) || "[]"
+    );
     return this.data;
   },
   create(name) {
-      // this.data = [{id:'1', name:'1'}, {id:'2', name:'2'}]
-    const names = this.data.map(item => item.name);
-    if (names.indexOf(name) >= 0) {return 'duplicated';}
-    this.data.push({id:name,name:name});
+    // this.data = [{id:'1', name:'1'}, {id:'2', name:'2'}]
+    const names = this.data.map((item) => item.name);
+    if (names.indexOf(name) >= 0) {
+      return "duplicated";
+    }
+    this.data.push({ id: name, name: name });
     this.save();
-    return 'success';
+    return "success";
+  },
+  update(id: string, name: string) {
+    const idList = this.data.map((item) => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.data.map((item) => item.name);
+      if (names.indexOf(name) >= 0) {
+        return "duplicated";
+      } else {
+        const tag = this.data.filter((item) => item.id === id)[0];
+        tag.name = name;
+        // tag.id = name;
+        this.save();
+        return "success";
+      }
+    } else {
+      return "not found";
+    }
+  },
+  remove(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    console.log(index)
+    this.data.splice(index, 1);
+    this.save();
+    return true;
   },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
-  }
+  },
 };
 export default tagListModel;
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,15 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left" />
+      <Icon @click="goBack" class="leftIcon" name="left" />
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem field-name="标签名" placeholder="请输入标签名" />
+      <FormItem :value="tag.name" @update:value="update" field-name="标签名" placeholder="请输入标签名" />
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -24,17 +24,34 @@ import Button from "@/components/Button.vue";
   components: { Button, FormItem }
 })
 export default class EditLabel extends Vue {
+  tag?: {id: string; name: string} = undefined;
+
   created() {
     const id = this.$route.params.id;//route.params.id就是路由里面的:id
     tagListModel.fetch();
     const tags = tagListModel.data;
     const tag = tags.filter(t => t.id === id)[0]; //过滤出位序和路由id一致的选项
     if (tag) {
-      console.log(tag);
+      this.tag = tag;
     } else {
       this.$router.replace("/404");
     }
   }
+
+update(name: string){
+  if(this.tag){
+    tagListModel.update(this.tag.id, name)
+  }
+}
+remove(){
+  if(this.tag){
+    tagListModel.remove(this.tag.id)
+  }
+}
+goBack(){
+  this.$router.back();
+}
+
 }
 </script>
 
